1eab769da6b92b760540c5802a782304906185de
Vy Tran <vtran1022@Gmail.com>
2021-05-24 13:54:17 -0700

    changed innerHTML to innertext for toggle func

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,13 +3,18 @@ console.log('hello world');
 // function to toggle the square to an x
 var toggleTic = (event) => {
   const square = event.target;
-  if (!square.innerHTML) {
-    square.innerHTML = 'x';
+  if (!square.innerText) {
+    square.innerText = 'x';
   } else {
-    square.innerHTML = "";
+    square.innerText = "";
   }
 };
 
 // event listener
 const squares = document.querySelectorAll('td');
 squares.forEach((square) => square.addEventListener("click", toggleTic));
+
+// const rows = document.querySelectorAll('tr');
+// rows.forEach((row) => {
+//   const square = row.children
+// })
