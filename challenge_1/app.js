d074a4659b55e9f89d900a2c0f022c4ac878e90f
Vy Tran <vtran1022@Gmail.com>
2021-05-24 11:41:49 -0700

    Started working on event listener

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,14 @@
 console.log('hello world');
 
+// function to toggle the square to an x
+var toggleTic = () => {
+  const square = document.getElementById('square');
+  console.log('func');
+  if (square.value === '') {
+    square.value = 'x';
+  }
+};
+
+// event listener
+const square = document.getElementById('square');
+square.addEventListener("click", toggleTic, false);
