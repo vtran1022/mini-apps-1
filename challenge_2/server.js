6640b3381e1769565fb26d362ecacb49e96fe964
Vy Tran <vtran1022@Gmail.com>
2021-05-25 11:14:33 -0700

    Created package.json, started port/express in server.js

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000 || process.env.port;
 
+app.post('/json_request', (req, res) => {
 
+})
+
+app.use(express.static('client'));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+})
