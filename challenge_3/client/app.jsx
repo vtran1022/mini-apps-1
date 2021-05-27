eefd78b737856f92abebe0e1bd3c85b38145359e
Vy Tran <vtran1022@Gmail.com>
2021-05-26 21:54:13 -0700

    Implemented code to get next butotn to render new form

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,68 +1,147 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   list
-    // };
+    this.state = {
+      userComponent: false,
+      shippingComponent: false,
+      billingComponent: false
+    };
+
+    this._onButton = this._onButton.bind(this);
+  }
+
+  _onButton(event) {
+    event.preventDefault();
+    if (event.target.id === 'checkout') {
+      this.setState({
+        userComponent: true
+      });
+    } else if (event.target.id === 'user') {
+    this.setState({
+      userComponent: false,
+      shippingComponent: true
+    });
+    } else if (event.target.id === 'ship') {
+      this.setState({
+        shippingComponent: false,
+        billingComponent: true
+      });
+    }
   }
 
   render() {
     return (
       <div className="home-page">
         <h1>Checkout</h1>
-        <button>Checkout</button>
+        <button id="checkout" onClick={this._onButton}>Checkout</button>
+        {this.state.userComponent
+          ? <User
+            next={this._onButton}
+            />
+          : null
+        }
+        {this.state.shippingComponent
+          ? <Shipping
+            next={this._onButton}
+            />
+          : null
+        }
+        {this.state.billingComponent
+          ? <Billing
+            next={this._onButton}
+            />
+          : null
+        }
       </div>
     )
   }
-};
+}
 
-class User extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: '',
-      email: '',
-      password: ''
-    };
-  }
-
-  render() {
-    return (
-      <div className="user">
-        <h3>User Information</h1>
-        <input className="name" id="user" type="text" placerholder="first and last name"/>
-        <button>Next</button>
+var User = ({ next }) => (
+  <div>
+    <h3>User Information</h3>
+    <form className="user">
+      <div>
+        <input id="name" type="text" placeholder="first and last name"/>
       </div>
-    )
-  }
-};
-
-class Shipping extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      line1: '',
-      line2: '',
-      city: '',
-      state: '',
-      zipcode: '',
-      phonenumber: '',
-    };
-  }
-
-  render() {
-    return (
-      <div className="shipping">
-        <h3>Shipping Information</h1>
-        <input class/>
-        <button>Next</button>
+      <div>
+      <input id="email" type="text" placeholder="email address"/>
       </div>
-    )
-  }
-};
+      <div>
+      <input id="password" type="text" placeholder="password"/>
+      </div>
+      <button id="user" onClick={next}>Next</button>
+    </form>
+  </div>
+)
+
+var Shipping = ({ next }) => (
+  <div>
+    <h3>Shipping Information</h3>
+    <form className="shipping">
+      <div>
+        <input id="line1" type="text" placeholder="address.."/>
+      </div>
+      <div>
+      <input id="line2" type="text" placeholder="address.."/>
+      </div>
+      <div>
+      <input id="city" type="text" placeholder="city"/>
+      </div>
+      <div>
+      <input id="state" type="text" placeholder="state"/>
+      </div>
+      <div>
+      <input id="zipcode" type="text" placeholder="zipcode"/>
+      </div>
+      <button id="ship" onClick={next}>Next</button>
+    </form>
+  </div>
+)
+
+var Billing = ({ next }) => (
+  <div>
+    <h3>Billing Information</h3>
+    <form className="billing">
+      <div>
+      <input id="credit-card" type="text" placeholder="credit card"/>
+      </div>
+      <div>
+      <input id="expiry-date" type="text" placeholder="expiration date"/>
+      </div>
+      <div>
+      <input id="CVV" type="text" placeholder="CVV"/>
+      </div>
+      <div>
+      <input id="billing-zipcode" type="text" placeholder="billing zipcode"/>
+      </div>
+      <button>Next</button>
+    </form>
+  </div>
+)
+
+// class Confirmation extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     // this.state = {
+//     //   list
+//     // };
+//   }
+
+//   render() {
+//     return (
+//       <div className="confirmation-page">
+//         <h1>Information Summary</h1>
+//         <button>Purchase</button>
+//         <App />
+//       </div>
+//     )
+//   }
+// }
 
 // at the end of components list -- render the app to the html view?
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<div>
+  <App />
+  </div>,
+  document.getElementById('app'));
 
