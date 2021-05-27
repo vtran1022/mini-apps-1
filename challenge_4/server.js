04f3a3291c8a3371ea1048d90a533f9fe053c226
Vy Tran <vtran1022@Gmail.com>
2021-05-27 10:00:06 -0700

    Set up server.js file to serve client files

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000 || process.env.port;
 
+app.use(express.json());
 
+// app.get('/', (req, res) => {
+//   res.send('Hello Boba');
+// })
+
+app.use(express.static('client/dist'));
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+})
